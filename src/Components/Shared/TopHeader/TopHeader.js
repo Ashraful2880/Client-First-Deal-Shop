f3d16cbfc99b6677bb21e22e9714d9a3cf9941d4
Ashraful <ashrafulislam2880@gmail.com>
2022-02-19 15:22:03 +0600

    Header Initialized

--- src/Components/Shared/TopHeader/TopHeader.js
+++ src/Components/Shared/TopHeader/TopHeader.js
@@ -2,8 +2,29 @@ import React from "react";
 
 const TopHeader = () => {
   return (
-    <div>
-      <h1 className="bg-gray-200">This is Top Header</h1>
+    <div className="border">
+      <div className="container mx-auto flex justify-between">
+        <div>
+          <ul className="flex">
+            <li>About Us</li>
+            <li>My Account</li>
+            <li>Order Tracking</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div>
+          <p>
+            <i className="fa-solid fa-truck" style={{ marginRight: "5px" }}></i>
+            100% Secure delivery in Courier
+          </p>
+        </div>
+        <div>
+          <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-twitter"></i>
+          <i className="fa-brands fa-instagram"></i>
+          <i className="fa-brands fa-whatsapp"></i>
+        </div>
+      </div>
     </div>
   );
 };
