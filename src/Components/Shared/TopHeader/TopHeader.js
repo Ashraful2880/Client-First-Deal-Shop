7389f7271c756be52d7c669bd39d9933f8bcb3b5
Ashraful <ashrafulislam2880@gmail.com>
2022-02-19 22:19:27 +0600

    Top Header Basic Part Added

--- src/Components/Shared/TopHeader/TopHeader.js
+++ src/Components/Shared/TopHeader/TopHeader.js
@@ -1,28 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const TopHeader = () => {
   return (
-    <div className="border">
+    <div className="border py-1">
       <div className="container mx-auto flex justify-between">
         <div>
-          <ul className="flex">
-            <li>About Us</li>
-            <li>My Account</li>
-            <li>Order Tracking</li>
-            <li>Contact Us</li>
-          </ul>
+          <Link className="mx-1 text-sm" to="/about">
+            About Us
+          </Link>
+          <Link className="mx-1 text-sm" to="/login">
+            My Account
+          </Link>
+          <Link className="mx-1 text-sm" to="/order">
+            Track Order
+          </Link>
+          <Link className="mx-1 text-sm" to="/contact">
+            Contact
+          </Link>
         </div>
         <div>
           <p>
-            <i className="fa-solid fa-truck" style={{ marginRight: "5px" }}></i>
+            <i className="fa-solid fa-truck mr-2"></i>
             100% Secure delivery in Courier
           </p>
         </div>
         <div>
-          <i className="fa-brands fa-facebook"></i>
-          <i className="fa-brands fa-twitter"></i>
-          <i className="fa-brands fa-instagram"></i>
-          <i className="fa-brands fa-whatsapp"></i>
+          <Link to="/home">
+            <i className="fa-brands fa-facebook mx-2"></i>
+          </Link>
+          <Link to="/home">
+            <i className="fa-brands fa-twitter mx-2"></i>
+          </Link>
+          <Link to="/home">
+            <i className="fa-brands fa-instagram mx-2"></i>
+          </Link>
+          <Link to="/home">
+            <i className="fa-brands fa-whatsapp mx-2"></i>
+          </Link>
         </div>
       </div>
     </div>
