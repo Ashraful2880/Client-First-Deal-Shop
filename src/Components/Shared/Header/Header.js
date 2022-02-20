30bc9ebc0e6ffe16adc72c1ff72e3e28cf56d7ef
Ashraful <ashrafulislam2880@gmail.com>
2022-02-20 20:57:44 +0600

    Header Menu added

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -8,12 +8,11 @@ const Header = () => {
     <div>
       <TopHeader />
       <MiddleHeader />
-
-      <nav className="bg-white shadow">
+      <nav className="bg-indigo-600 shadow">
         <div className="container mx-auto px-6 py-3 md:flex md:justify-between md:items-center">
           <div className="flex justify-between items-center">
-            <div>
-              <Link to="/home" className="text-gray-800 hover:text-gray-700">
+            <div className="lg:block sm:hidden md:hidden">
+              <Link to="/home" className="text-white hover:text-gray-700">
                 <i className="fa-solid fa-bars mr-2"></i>
                 Browse Categories
                 <i className="fa-solid fa-angle-down ml-2"></i>
@@ -23,53 +22,50 @@ const Header = () => {
             <div className="flex md:hidden">
               <button
                 type="button"
-                className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600"
+                className="text-white hover:text-gray-600 focus:outline-none focus:text-gray-600"
                 aria-label="toggle menu"
               >
-                <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
-                  <path
-                    fill-rule="evenodd"
-                    d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                  ></path>
-                </svg>
+                <i className="fa-solid fa-bars mr-2"></i>
               </button>
             </div>
           </div>
 
           <div className="md:flex items-center">
-            <div className="flex flex-col md:flex-row md:mx-6">
+            <div className="flex flex-col md:flex-row md:mx-6 text-white">
               <Link
-                className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0"
+                className="my-1 text-sm font-medium hover:text-indigo-500 md:mx-4 md:my-0"
                 to="/home"
               >
                 Home
               </Link>
               <Link
-                className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0"
+                className="my-1 text-sm font-medium hover:text-indigo-500 md:mx-4 md:my-0"
                 to="/home"
               >
                 Categories
+                <i className="fa-solid fa-angle-down ml-2"></i>
               </Link>
               <Link
-                className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0"
+                className="my-1 text-sm font-medium hover:text-indigo-500 md:mx-4 md:my-0"
                 to="/shop"
               >
                 Shop
+                <i className="fa-solid fa-angle-down ml-2"></i>
               </Link>
               <Link
-                className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0"
+                className="my-1 text-sm font-medium hover:text-indigo-500 md:mx-4 md:my-0"
                 to="/contact"
               >
                 Contact
               </Link>
               <Link
-                className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0"
+                className="my-1 text-sm font-medium hover:text-indigo-500 md:mx-4 md:my-0"
                 to="/about"
               >
                 About
               </Link>
               <Link
-                className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0"
+                className="my-1 text-sm font-medium hover:text-indigo-500 md:mx-4 md:my-0"
                 to="/blogs"
               >
                 Blog
--- src/Components/Shared/MiddleHeader/MiddleHeader.js
+++ src/Components/Shared/MiddleHeader/MiddleHeader.js
@@ -4,40 +4,45 @@ import logo from "../../../Images/Mainlogo.png";
 
 const MiddleHeader = () => {
   return (
-    <div className="container mx-auto flex justify-between items-center">
-      <div>
-        <Link to="/home">
-          <img className="w-32" src={logo} alt="Main Logo" />
-        </Link>
-      </div>
-      <div className="container w-full md:max-w-3xl mx-auto flex flex-col">
-        <form action="/search">
-          <div className="bg-white flex items-center rounded-lg shadow-lg md:shadow-xl border border-red-500 h-11">
-            <input
-              autoFocus
-              type="search"
-              className="rounded-l-full w-full  px-6 text-gray-700 leading-tight focus:outline-none"
-              id="search"
-              placeholder="Search Product By Name"
-            />
-            <div className="p-2 md:p-4">
-              <button className="rounded-full focus:outline-none w-10 h-12 md:w-10 md:h-12 flex items-center justify-center">
-                <i className="fa-solid fa-magnifying-glass"></i>
-              </button>
+    <div className="border-b bg-indigo-100">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          <Link to="/home">
+            <img className="w-32" src={logo} alt="Main Logo" />
+          </Link>
+        </div>
+        <div className="container mx-auto w-3/6 ">
+          <form action="/search">
+            <div className="bg-white flex items-center rounded-lg border h-12 shadow-lg">
+              <input
+                autoFocus
+                type="search"
+                className="w-full  px-6 text-gray-700 leading-tight focus:outline-none"
+                placeholder="Search Product By Name"
+              />
+              <div className="p-2 md:p-4">
+                <button className="rounded-full focus:outline-none w-10 h-12 md:w-10 md:h-12 flex items-center justify-center">
+                  <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
-      </div>
-      <div>
-        <Link to="/home" className="relative">
-          <i className="fa-solid fa-bag-shopping mx-2 text-2xl text-gray-700  px-2"></i>
-          <span className="absolute bottom-3 left-10 rounded-full bg-indigo-500 text-white px-1 text-xs">
-            1
-          </span>
-        </Link>
-        <i className="fa-solid fa-user-large mx-2 text-2xl text-gray-700"></i>
-        <i className="fa-solid fa-headphones mx-2 text-2xl text-gray-700"></i>
-        <span>+88-01974238487</span>
+          </form>
+        </div>
+        <div>
+          <Link to="/home" className="relative">
+            <i className="fa-solid fa-cart-shopping mx-2 text-2xl text-gray-700  px-2"></i>
+            <span className="absolute bottom-4 left-11 rounded-full bg-indigo-500 text-white px-1 text-xs">
+              1
+            </span>
+          </Link>
+          <Link to="/login">
+            <i className="fa-solid fa-user-large mx-2 text-2xl text-gray-700"></i>
+          </Link>
+          <Link to="contact">
+            <i className="fa-solid fa-headphones mx-2 text-2xl text-gray-700"></i>
+            <span>+88-01974238487</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
