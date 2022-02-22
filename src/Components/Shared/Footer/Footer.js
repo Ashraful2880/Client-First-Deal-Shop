06ea66f728e0233281d96e1823ad2edeb35745b0
Ashraful <ashrafulislam2880@gmail.com>
2022-02-22 14:57:25 +0600

    Footer Added

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -5,100 +5,111 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <div className="bg-gray-800">
+    <div className="footer-container">
+      <div>
         <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-center py-6 ">
-          <div>
-            <img className="w-48" src={logo} alt="Main Logo" />
-          </div>
-          <div>
+          <Link to="/home" className=" mb-2">
+            <img className=" w-16" src={logo} alt="Main Logo" />
+            <h4 className="text-orange-500 text-left font-bold mt-2">
+              First Deal Shop
+            </h4>
+          </Link>
+          <div className="flex mb-2">
             <p>
-              <i className="fa-solid fa-location-dot bg-white p-5 rounded-full text-indigo-600"></i>
+              <i className="fa-solid fa-location-dot bg-orange-500 hover:bg-white hover:text-orange-500 duration-300 p-3 rounded-full text-white mr-5"></i>
             </p>
-            <p className="text-white">No: 58 A, East Madison Street,</p>
-            <p className="text-white">Baltimore, MD, USA 4508.</p>
+            <div>
+              <p className="text-white hover:text-orange-500 duration-300 cursor-pointer text-left">
+                No: 58 A, East Madison Street,
+              </p>
+              <p className="text-white hover:text-orange-500 duration-300 cursor-pointer text-left">
+                Baltimore, MD, USA 4508.
+              </p>
+            </div>
           </div>
-          <div className="flex justify-center">
-            <i className="fa-solid fa-phone-flip bg-white p-5 rounded-full text-indigo-600"></i>
-            <h2 className="text-white">+123-456-7890</h2>
+          <div className="flex items-center mb-2">
+            <i className="fa-solid fa-phone-flip bg-orange-500 hover:bg-white hover:text-orange-500 duration-300 p-3 rounded-full text-white mr-5"></i>
+            <h2 className="text-white hover:text-orange-500 duration-300 cursor-pointer">
+              +123-456-7890
+            </h2>
           </div>
-          <div>
+          <div className="text-left lg:text-center mb-2">
             <Link to="/home">
-              <i className="fa-brands fa-facebook mx-2 hover:text-red-600 bg-white p-3 rounded-full text-indigo-600"></i>
+              <i className="fa-brands fa-facebook mx-2 bg-orange-500 hover:bg-white hover:text-orange-500 duration-300 p-3 rounded-full text-white"></i>
             </Link>
             <Link to="/home">
-              <i className="fa-brands fa-twitter mx-2 hover:text-red-600 bg-white p-3 rounded-full text-indigo-600"></i>
+              <i className="fa-brands fa-twitter mx-2 bg-orange-500 hover:bg-white hover:text-orange-500 duration-300 p-3 rounded-full text-white"></i>
             </Link>
             <Link to="/home">
-              <i className="fa-brands fa-instagram mx-2 hover:text-red-600 bg-white p-3 rounded-full text-indigo-600"></i>
+              <i className="fa-brands fa-instagram mx-2 bg-orange-500 hover:bg-white hover:text-orange-500 duration-300 p-3 rounded-full text-white"></i>
             </Link>
             <Link to="/home">
-              <i className="fa-brands fa-whatsapp mx-2 hover:text-red-600 bg-white p-3 rounded-full text-indigo-600"></i>
+              <i className="fa-brands fa-whatsapp mx-2 bg-orange-500 hover:bg-white hover:text-orange-500 duration-300 p-3 rounded-full text-white"></i>
             </Link>
           </div>
         </div>
       </div>
       <hr />
-      <div className="footer-container">
+      <div>
         <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 pt-6 pb-10 text-white">
           <div>
             <h4 className="mb-4 text-xl font-bold">Legal</h4>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Terms Of Service</p>
             </Link>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Privacy Policy</p>
             </Link>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Cookies</p>
             </Link>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Ad Cookies</p>
             </Link>
           </div>
 
           <div>
             <h4 className="mb-4 text-xl font-bold">Partners</h4>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Retailers</p>
             </Link>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Affiliate</p>
             </Link>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Developers</p>
             </Link>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Press Release</p>
             </Link>
           </div>
           <div>
             <h4 className="mb-4 text-xl font-bold">Service</h4>
-            <Link to="/about" className="hover:text-red-600">
+            <Link to="/about" className="hover:text-orange-500 duration-300">
               <p>About Us</p>
             </Link>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Careers</p>
             </Link>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Buzz</p>
             </Link>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Activity Index</p>
             </Link>
           </div>
           <div>
             <h4 className="mb-4 text-xl font-bold">Information</h4>
-            <Link to="/about" className="hover:text-red-600">
+            <Link to="/about" className="hover:text-orange-500 duration-300">
               <p>Company</p>
             </Link>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Partners</p>
             </Link>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Programmes</p>
             </Link>
-            <Link to="/home" className="hover:text-red-600">
+            <Link to="/home" className="hover:text-orange-500 duration-300">
               <p>Members</p>
             </Link>
           </div>
