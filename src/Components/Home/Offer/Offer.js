e640e88d0f4304efdf08731c82d84dcbe59cd5b2
Ashraful <ashrafulislam2880@gmail.com>
2022-02-25 17:23:15 +0600

    404 Page Design Complete

--- src/Components/Home/Offer/Offer.js
+++ src/Components/Home/Offer/Offer.js
@@ -5,21 +5,21 @@ const Offer = () => {
   return (
     <div className=" my-10 container mx-auto border shadow-md p-5">
       <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 gap-5">
-        <Link to="/home" className="rounded-2xl overflow-hidden">
+        <Link to="/others" className="rounded-2xl overflow-hidden">
           <img
             className="hover:scale-110 duration-500 rounded-r-xl"
             src="https://i.ibb.co/wKDh9nL/Offer-1.jpg"
             alt="OfferImage"
           />
         </Link>
-        <Link to="/home" className=" rounded-2xl overflow-hidden">
+        <Link to="/women" className=" rounded-2xl overflow-hidden">
           <img
             className="hover:scale-110 duration-500 rounded-r-xl"
             src="https://i.ibb.co/X4qSh3B/Offer-2.jpg"
             alt="OfferImage"
           />
         </Link>
-        <Link to="/home" className=" rounded-2xl overflow-hidden">
+        <Link to="/uncategorized" className=" rounded-2xl overflow-hidden">
           <img
             className="hover:scale-110 duration-500 rounded-r-xl"
             src="https://i.ibb.co/rFnS4TT/offer-3.jpg"
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Opps... Page Not Found</h1>
+    <div className=" w-4/6 mx-auto py-24 bg-gray-100 rounded-xl my-10">
+      <img
+        className="mx-auto"
+        src="https://i.ibb.co/ThGcj8s/404.png"
+        alt="Error Page"
+      />
+      <p className="text-gray-600 text-lg mt-4 mb-10">
+        We Cannot Find The Page You Are Looking For
+      </p>
+      <Link to="/home">
+        <button className="bg-orange-500 px-3 py-2 border border-orange-500 rounded-lg font-bold text-white hover:bg-transparent hover:text-orange-500 duration-300">
+          Go to Home
+        </button>
+      </Link>
     </div>
   );
 };
