8a81ec1f1efd1120dd48195d6181b70c6e926f53
Md Ashraful Islam <ashrafulislam2880@gmail.com>
2022-03-13 17:54:02 +0600

    Newlater Page Design Added

--- src/Components/Home/Newslater/Newslater.js
+++ src/Components/Home/Newslater/Newslater.js
@@ -3,48 +3,30 @@ import "./Newslater.css";
 
 const Newslater = () => {
   return (
-    <div className="container mx-auto">
-      <div className="flex justify-center px-6 mb-12 pt-20">
-        <div className="w-full flex">
-          <div className="w-full bg-transparent hidden lg:block bg-cover rounded-l-lg subscribeContainer"></div>
-          <div className="w-full lg:w-7/12 p-5 rounded-lg lg:rounded-l-none pb-20 bg-gray-700">
-            <h1 className="pt-10 text-5xl text-center text-orange-500 font-semibold leading-tight">
-              Subscribe &
-            </h1>
-            <h1 className="pt-1 text-4xl text-center text-white font-semibold  leading-tight">
-              Get 15%
-            </h1>
-            <h1 className="pt-1 text-5xl text-center text-orange-500 font-semibold leading-tight">
-              Discount
-            </h1>
-            <form className=" px-4 pt-6 pb-2 mb-4 text-center">
-              <div className="mb-3 w-full mx-auto">
-                <label
-                  className="block mb-2 text-xl font-bold text-white text-left"
-                  htmlFor="email"
-                >
-                  Email
-                </label>
-                <input
-                  className="w-full p-4 text-lg leading-tight text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline mb-4"
-                  name="email"
-                  type="email"
-                  placeholder="Subscribe Our Newslater"
-                />
-              </div>
-              <div className="ml-2 text-center">
-                <button
-                  className=" lg:w-1/3 sm:w-full md:w-full px-4 py-3 font-bold text-white bg-orange-500 border border-orange-500 rounded-lg hover:bg-transparent duration-300"
-                  type="submit"
-                >
-                  Subscribe
-                </button>
-              </div>
-            </form>
-            <div className="text-white mt-6">
-              <h3 className="text-4xl mb-3">Alwayes Get Up to date</h3>
-              <h3 className="text-2xl">With Best Deal Offer</h3>
-            </div>
+    <div className="flex justify-center mb-12 pt-20 ">
+      <div className="w-full flex">
+        <div className="w-full hidden lg:block bg-cover rounded-l-lg subscribeContainer"></div>
+        <div className="w-full lg:w-7/12 p-5 rounded-lg lg:rounded-l-none pb-20 bg-gray-900">
+          <h1 className="pt-10 text-4xl text-center text-orange-500 font-semibold leading-tight">
+            Subscribe Newslater
+          </h1>
+          <form className="pt-6 text-center flex justify-between items-center">
+            <input
+              className="w-full px-4 py-3 text-sm leading-tight text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline mx-1"
+              name="email"
+              type="email"
+              placeholder="Subscribe Our Newslater"
+            />
+            <button
+              className="px-10 py-2 font-bold text-white bg-orange-500 border border-orange-500 rounded-lg hover:bg-transparent duration-300 mx-1"
+              type="submit"
+            >
+              Subscribe
+            </button>
+          </form>
+          <div className="text-white mt-10 text-center">
+            <h3 className="text-4xl mb-2">Alwayes Get Up to date</h3>
+            <h3 className="text-xl">With Best Deal Offer</h3>
           </div>
         </div>
       </div>
