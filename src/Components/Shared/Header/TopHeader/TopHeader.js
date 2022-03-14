f1c3f8f1ade35613bf8507910b0d49d421eb7d8e
Md Ashraful Islam <ashrafulislam2880@gmail.com>
2022-03-14 13:58:14 +0600

    Top Header Responsive Issue Fixed

--- src/Components/Shared/Header/TopHeader/TopHeader.js
+++ src/Components/Shared/Header/TopHeader/TopHeader.js
@@ -3,32 +3,32 @@ import { Link } from "react-router-dom";
 
 const TopHeader = () => {
   return (
-    <div className="py-1 bg-orange-600 text-white border-t-2">
-      <div className="container mx-auto flex justify-between">
+    <div className="py-1 bg-orange-600 text-white">
+      <div className="container mx-auto flex justify-between items-center">
         <div>
           <Link
-            className="mx-1 text-sm hover:text-black duration-200"
+            className="mx-1 text-xs lg:text-base md:text-base hover:text-black duration-200 border-r border-gray-300"
             to="/about"
           >
             About Us
-            <span className=" h-full w-[1px] bg-gray-300 inline-block align-middle ml-2"></span>
+            <span className="ml-2"></span>
           </Link>
           <Link
-            className="mx-1 text-sm hover:text-black duration-200"
+            className="mx-1 text-xs lg:text-base md:text-base hover:text-black duration-200 border-r border-gray-300"
             to="/login"
           >
             My Account
-            <span className=" h-full w-[1px] bg-gray-300 inline-block align-middle ml-2"></span>
+            <span className="ml-2"></span>
           </Link>
           <Link
-            className="mx-1 text-sm hover:text-black duration-200 "
+            className="mx-1 text-xs lg:text-base md:text-base hover:text-black duration-200 border-r border-gray-300"
             to="/order"
           >
             Track Order
-            <span className=" h-full w-[1px] bg-gray-300 inline-block align-middle ml-2"></span>
+            <span className="ml-2"></span>
           </Link>
           <Link
-            className="mx-1 text-sm hover:text-black duration-200"
+            className="mx-1 text-xs lg:text-base md:text-base hover:text-black duration-200 hidden lg:inline-block"
             to="/contact"
           >
             Contact
@@ -36,16 +36,16 @@ const TopHeader = () => {
         </div>
         <div>
           <a href="https://www.facebook.com/ashrafonline706/" target="blank">
-            <i className="text-lg fa-brands fa-facebook mx-2 hover:text-black duration-300"></i>
+            <i className="fa-brands fa-facebook lg:text-lg md:text-base sm:text-sm text-xs mx-2 hover:text-black duration-300"></i>
           </a>
           <a href="https://twitter.com/IslamAshraful80" target="blank">
-            <i className="text-lg fa-brands fa-twitter mx-2 hover:text-black duration-300"></i>
+            <i className="fa-brands fa-twitter lg:text-lg md:text-base sm:text-sm text-xs mx-2 hover:text-black duration-300"></i>
           </a>
           <a href="https://www.instagram.com/ashraful.islam706/" target="blank">
-            <i className="text-lg fa-brands fa-instagram mx-2 hover:text-black duration-300"></i>
+            <i className="fa-brands fa-instagram lg:text-lg md:text-base sm:text-sm text-xs mx-2 hover:text-black duration-300"></i>
           </a>
           <a href="https://www.facebook.com/ashrafonline706/" target="blank">
-            <i className="text-lg fa-brands fa-whatsapp mx-2 hover:text-black duration-300"></i>
+            <i className="fa-brands fa-whatsapp lg:text-lg md:text-base sm:text-sm text-xs mx-2 hover:text-black duration-300"></i>
           </a>
         </div>
       </div>
