59453f68ad3e042d9a9785e28a280947b478998d
Md Ashraful Islam <ashrafulislam2880@gmail.com>
2022-03-14 14:36:45 +0600

    middle Header Responsive Issue Fixed

--- src/Components/Shared/Header/MiddleHeader/MiddleHeader.js
+++ src/Components/Shared/Header/MiddleHeader/MiddleHeader.js
@@ -15,31 +15,29 @@ const MiddleHeader = () => {
 
   return (
     <div className="bg-gray-900 border-b border-gray-500">
-      <div className="container mx-auto flex justify-between items-center py-2">
-        <Link to="contact" className="ml-4">
-          <i className="fa-solid fa-phone mx-2 text-xl text-orange-500"></i>
-          <span className=" font-bold text-orange-500">
+      <div className="container mx-auto lg:flex sm:block justify-between items-center py-1">
+        <span className="ml-4">
+          <i className="fa-solid fa-phone mx-2 lg:text-xl text-base text-orange-500"></i>
+          <span className="font-bold text-orange-500">
             Hotline: +88-01974238487
           </span>
-        </Link>
-        <div className="container mx-auto w-2/6 ">
-          <form>
-            <div className="flex justify-center py-1">
-              <input
-                type="search"
-                className="w-full px-3 py-2 text-gray-700 bg-white border border-solid border-gray-300 rounded rounded-r-none transition ease-in-out focus:bg-gray-100"
-                placeholder="I'm looking For"
-              />
-              <button
-                className="btn px-6 py-2 bg-orange-500 text-black font-bold rounded rounded-l-none hover:bg-white duration-150 ease-in-out border-l border-orange-500"
-                type="button"
-              >
-                <i className="fa-solid fa-magnifying-glass"></i>
-              </button>
-            </div>
+        </span>
+        <div className="container mx-auto lg:w-2/6 w-5/6">
+          <form className="flex justify-center py-1">
+            <input
+              type="search"
+              className="w-full px-3 py-2 text-gray-700 bg-white border border-solid border-gray-300 rounded rounded-r-none transition ease-in-out focus:bg-gray-100"
+              placeholder="I'm looking For"
+            />
+            <button
+              className="btn px-6 py-2 bg-orange-500 text-black font-bold rounded rounded-l-none hover:bg-white duration-150 ease-in-out border-l border-orange-500"
+              type="button"
+            >
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
           </form>
         </div>
-        <div>
+        <div className="mt-1 lg:mt-0 flex items-center justify-center">
           <Link to="/faviorite" className="relative">
             <i className="fa-solid fa-heart text-orange-500 text-2xl mx-4 hover:text-white duration-200"></i>
             <span
@@ -59,13 +57,13 @@ const MiddleHeader = () => {
             </span>
           </Link>
           {user ? (
-            <button className="text-md font-semibold hover:text-orange-500 focus:text-orange-500 duration-300 md:mx-4 md:my-0 ml-6">
+            <Link to="/">
               <img
-                className="w-8 rounded-full border-2 border-white mx-auto ml-6 "
+                className="w-10 rounded-full border-2 border-white mx-auto ml-6 inline-b"
                 src={user?.photoURL}
                 alt={"user"}
               />
-            </button>
+            </Link>
           ) : (
             <Link to="/login">
               <i className="fa-solid fa-right-to-bracket mr-2 ml-6 text-xl text-white"></i>
